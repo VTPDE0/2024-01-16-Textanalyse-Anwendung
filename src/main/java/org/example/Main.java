30f0e561564687e88a5dd03a0a698988bf777dc6
vtp <vtp@ordix.de>
2024-01-16 16:32:15 +0100

    v0.2 Eingabe-Scanner, If Bedingung zum Programm beenden und einige Ausgaben dafür sind erzeugt. Auch Zeichenzähler ist da

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Textanalyse-Anwendung!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gib einen Text ein (Tippe 'exit' zum Beenden):");
+
+        String text = scanner.nextLine();
+
+        // Beenden des Programms, wenn der Benutzer "exit" eingibt
+        if(text.equalsIgnoreCase("exit")) {
+            System.out.println("Programm beendet.");
+            return;
+        }
+
+        // Zeichenzählung
+        int zeichenAnzahl = text.length();
+
+
+        // Ausgabe der Ergebnisse
+        System.out.println("Anzahl der Zeichen: " + zeichenAnzahl);
+
+        scanner.close();
     }
 }
